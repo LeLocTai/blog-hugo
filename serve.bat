6f1b1f75a89e4127f47e7d8168a683a03258bc67
Tai Le <vzttzv@gmail.com>
2020-11-18 17:13:46 +0700

    show qr code when serve

--- serve.bat
+++ serve.bat
@@ -2,4 +2,8 @@
 
 for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
 
-hugo server --buildDrafts --buildFuture --bind=0.0.0.0 --baseURL="http://%NetworkIP%/"
+set url="http://%NetworkIP%"
+
+qrc -i %url%:1313
+
+hugo server --buildDrafts --buildFuture --bind=0.0.0.0 --baseURL=%url%
